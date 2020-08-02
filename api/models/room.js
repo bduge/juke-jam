d9f6ed9a65ceba2bad092afdb6acc9621ef4a60f
Bill DuGe <billduge11@gmail.com>
2020-08-02 17:37:30 -0600

    Updated spotify get token endpoint

--- api/models/room.js
+++ api/models/room.js
@@ -2,9 +2,11 @@ var mongoose = require("mongoose");
 
 let roomSchema = new mongoose.Schema({
 	name: { type: String, required: true },
+	room_owner_email: { type: String, require: true },
 	access_token: String,
 	refresh_token: String,
 	generated_time: Date,
+	expires_in: Number,
 });
 
 module.exports = mongoose.model("Room", roomSchema);
--- api/routes/spotify.js
+++ api/routes/spotify.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var router = express.Router();
 const dotenv = require("dotenv");
-const request = require("request");
+const axios = require("axios");
 const Room = require("../models/room");
 
 dotenv.config();
+
+const scopes = "user-read-private user-read-email";
 
 // TEST ENDPOINT, call will be made from frontend
 router.get("/authorize", function (req, res) {
@@ -12,19 +14,22 @@ router.get("/authorize", function (req, res) {
 		"https://accounts.spotify.com/authorize?response_type=code" +
 			"&client_id=" +
 			process.env.CLIENT_ID +
+			"&scope=" +
+			encodeURIComponent(scopes) +
 			"&redirect_uri=http://localhost:8000/spotify/get_token"
 	);
 });
 
 // Retrieve authorization and refresh token from backend. Store in database under room name
-router.get("/get_token", function (req, res) {
-	let code = req.query.code;
-	let room = req.query.room;
+router.get("/get_token", async function (req, res) {
+	let code = req.query.token;
+	let room = req.body.roomName || "Bill's room";
 
-	const options = {
+	const token_options = {
+		method: "post",
+		headers: { "Content-Type": "application/x-www-form-urlencoded" },
 		url: "https://accounts.spotify.com/api/token",
-		method: "POST",
-		form: {
+		params: {
 			grant_type: "authorization_code",
 			code: code,
 			redirect_uri: "http://localhost:8000/spotify/get_token",
@@ -32,33 +37,63 @@ router.get("/get_token", function (req, res) {
 			client_secret: process.env.CLIENT_SECRET,
 		},
 	};
+	let body;
+	try {
+		let response = await axios(token_options);
+		body = response.data;
+	} catch (error) {
+		console.log(error);
+		res.send(ERROR);
+		return;
+	}
 
-	request(options, function (err, response, body) {
-		body = JSON.parse(body);
-		console.log(body);
-		if (body.error) {
-			console.log(body.error_description);
+	let access_token = body.access_token;
+	let email;
+	try {
+		email = await get_user(access_token);
+	} catch (error) {
+		console.log(error);
+		res.send("ERROR");
+		return;
+	}
+
+	let new_room = new Room({
+		name: room,
+		room_owner_email: email,
+		access_token: access_token,
+		refresh_token: body.refresh_token,
+		expires_in: body.expires_in,
+		generated_time: Date.now(),
+	});
+	new_room.save((err, room) => {
+		if (err) {
+			console.log(err);
 			res.send("ERROR");
 		} else {
-			let newRoom = new Room({
-				name: room,
-				access_token: body.access_token,
-				refresh_token: body.refresh_token,
-				generated_time: Date.now(),
-			});
-			newRoom.save((err, room) => {
-				if (err) {
-					console.log(err);
-					res.send("ERROR");
-				} else {
-					console.log(room);
-					res.send("SUCCESS");
-				}
-			});
+			res.send("SUCCESS");
 		}
 	});
 });
 
-function refresh_token(refresh_key) {}
+// Get users email from spotify api
+async function get_user(access_token) {
+	const user_options = {
+		url: "https://api.spotify.com/v1/me",
+		headers: {
+			Authorization: "Bearer " + access_token,
+		},
+	};
+	let response;
+	try {
+		response = await axios(user_options);
+	} catch (error) {
+		console.log(error);
+		return false;
+	}
+	let body = response.data;
+	return body.email;
+}
+
+function refresh_token(room_name) {}
 
 module.exports = router;
