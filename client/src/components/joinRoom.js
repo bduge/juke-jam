34f1123d47399599e008290045354f0429be6802
Tony Wang <tonyy.wangg@gmail.com>
2020-08-26 21:07:58 -0400

    Fixed master

--- client/src/components/joinRoom.js
+++ client/src/components/joinRoom.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Header, Input, Button, Grid} from 'semantic-ui-react';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import './styles.css';
 import {socket} from './socketConnection';
 
@@ -10,29 +10,12 @@ export default class JoinRoom extends React.Component {
         this.state = {
             roomName: "",
         }
-        this.callbackFunc = this.callbackFunc.bind(this);
     }
 
     onSetName = (e, {value}) => {
         this.setState({
             roomName: value, 
         });
-    }
-
-    callbackFunc = (didPass) => {
-        if(didPass) {
-            <Redirect to ={{
-                pathname:'/new-room',
-                state:{
-                    isHost : false, 
-                    roomName: this.state.roomName,
-                }
-            }}/>
-        } 
-    }
-
-    joinRoom = () => {
-        socket.emit("request join", this.state.roomName, callbackFunc);
     }
 
     render() {
