fc743e3f5ede81285944232d32f5909cf9cf72d1
Bill DuGe <billduge11@gmail.com>
2020-08-31 02:54:55 -0600

    Created song component with placeholders, added disconnecting socket event to check for empty room

--- api/socket.js
+++ api/socket.js
@@ -1,47 +1,62 @@
-var socketio = require("socket.io");
-const Room = require("./models/room");
+var socketio = require('socket.io')
+const Room = require('./models/room')
 
 function connectSocket(server) {
-	io = socketio(server);
-	io.on("connection", (socket) => {
-		console.log("A user connected: " + socket.id);
+    io = socketio(server)
+    io.on('connection', (socket) => {
+        console.log('A user connected: ' + socket.id)
 
-		// Disconnect
-		socket.on("disconnect", () => {
-			console.log("User disconnected");
-		});
+        // Disconnect
+        socket.on('disconnect', () => {
+            console.log(socket.id, 'disconnected')
+        })
 
-		// Join Room
-		socket.on("request join", async (roomName, callback) => {
-			if (roomName == "") {
-				callback(false, "Invalid room name");
-			} else if (Object.keys(socket.rooms).length > 1) {
-				callback(false, "Already in room");
-			} else {
-				let existing = await Room.findOne({ name: roomName }).exec();
-				if (!existing) {
-					callback(false, "Room doesn't exist");
-				} else {
-					socket.join(roomName);
-					io.sockets.in(roomName).emit('joinedRoom', "Someone has joined the room");
-					callback(true, "Joined " + roomName);
-				}
-			}
-		});
+        // Before disconnect, rooms still visible
+        socket.on('disconnecting', () => {
+            for (const room in socket.rooms) {
+                if (room == socket.id) {
+                    continue
+                }
+                console.log(socket.id, 'is leaving', room)
+                // TODO: Check if room is empty, delete if true
+            }
+        })
 
-		// Leave Room
-		socket.on("request leave", (roomName, callback) => {
-			console.log(roomName);
-			socket.leave(roomName);
-			callback(true, "Left " + roomName);
-		});
+        // Join Room
+        socket.on('request join', async (roomName, callback) => {
+            if (roomName == '') {
+                callback(false, 'Invalid room name')
+            } else if (Object.keys(socket.rooms).length > 1) {
+                callback(false, 'Already in room')
+            } else {
+                let existing = await Room.findOne({ name: roomName }).exec()
+                if (!existing) {
+                    callback(false, "Room doesn't exist")
+                } else {
+                    socket.join(roomName)
+                    io.sockets
+                        .in(roomName)
+                        .emit('joinedRoom', 'Someone has joined the room')
+                    callback(true, 'Joined ' + roomName)
+                }
+            }
+        })
 
-		// Chat feature (testing purposes)
-		socket.on("chat message", (msg, room) => {
-			console.log(socket.rooms);
-			io.to(room).emit("chat message", msg);
-		});
-	});
+        // Leave Room
+        socket.on('request leave', (roomName, callback) => {
+            console.log(socket.id, 'is leaving', roomName)
+            socket.leave(roomName)
+            if (callback) {
+                callback(true, 'Left ' + roomName)
+            }
+        })
+
+        // Chat feature (testing purposes)
+        socket.on('chat message', (msg, room) => {
+            console.log(socket.rooms)
+            io.to(room).emit('chat message', msg)
+        })
+    })
 }
 
-module.exports = connectSocket;
+module.exports = connectSocket
