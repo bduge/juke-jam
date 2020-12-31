f076e7565042d39ca363a6cf5e4bb933b7ec75d7
Bill DuGe <billduge11@gmail.com>
2020-12-30 23:35:50 -0700

    Styled navbar and made responsive

--- client/src/components/room.js
+++ client/src/components/room.js
@@ -177,27 +177,32 @@ class Room extends React.Component {
                         <Link to="/">
                             <Button onClick={this.deleteLocalStorage}>Leave</Button>
                         </Link>
-                        {/* {this.state.isHost ? ( */}
-                            <Popup
-                            content='Link Copied'
-                            on='click'
-                            pinned
-                            trigger={<Button onClick={this.saveLink} content='Get Shareable Link' />}
-                            />
+                        {this.state.isHost ? (
                         <DeviceModal
                             roomName={this.state.roomName}
                             saveDevice={this.saveDevice}
                         />
-                        {/* ) : ( */}
-                        {/* <></> */}
-                        {/* )} */}
+                        ) : (
+                        <></>
+                        )}
                     </div>
-                    <Header
-                        className="headerText"
-                        textAlign={'center'}
-                        as="h3"
-                        content={this.state.roomName.toUpperCase()}
-                    />
+                    <div className='header'>
+                        <Header
+                            className="headerText"
+                            textAlign={'center'}
+                            as="h3"
+                            content={this.state.roomName.toUpperCase()}
+                        />
+                        <Popup
+                            content='Link Copied'
+                            on='click'
+                            pinned
+                            trigger={
+                                <Icon className='linkButton' name='linkify' onClick={this.saveLink}/>
+                            }
+                        />  
+                    </div>
+                    
                     
                     <Grid stackable>
                     <Grid.Column width={5}>
